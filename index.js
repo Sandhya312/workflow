44dc46f20ee4c91e3ceb3f2a334723f3d6e1dd4c
Sandhya312 <sandhyakumari3122003@gmail.com>
2024-03-22 02:02:57 +0530

    added static upload folder

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const workflowRoutes = require('./routes/workflowRoutes');
-
+const path = require('path');
 connectDB();
 
 const app = express();
@@ -16,7 +16,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(cors("Access-Control-Allow-Origin:*")); // Enable CORS for all origins
-
+// Serve static files from the 'uploads' directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/workflow',workflowRoutes);
 
 // Start the server and listen on the specified port
